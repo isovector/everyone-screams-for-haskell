9f84c62e448facdcef3957e45a8ab29f59d5c91c
DebugSteven <debugsteven@gmail.com>
2017-07-09 15:22:44 -0600

    parse functions

--- src/Icecream.hs
+++ src/Icecream.hs
@@ -7,11 +7,17 @@ import BasePrelude hiding (try)
 
 
 data TypeDecl = TypeDecl { tdId :: String, tdType :: Type }
+          deriving Show
 
 data Type = TypeCtr String
           | TypeArr Type Type
           deriving Show
 
+data Func = Func { fID :: String, fBinds :: [String], fBody :: Expr }
+          deriving Show
+
+type Expr = String
+           
 
 typeParser :: Parser Type
 typeParser = do
@@ -32,12 +38,26 @@ ident = trimming $ do
   return $ c : cs
   
 
-icecream :: Parser Type
-icecream = typeParser
+icecream = parseFunc
 
+parseTypeD :: Parser TypeDecl
+parseTypeD = do
+    a <- ident
+    trimming $ char ':'
+    b <- typeParser
+    return $ TypeDecl a b
+
+parseFunc :: Parser Func
+parseFunc = do 
+    a <- ident
+    b <- many $ ident
+    trimming $ char '='
+    c <- ident
+    return $ Func a b c
 
 main :: IO ()
 main = do
   f <- readFile "src/test.icecream"
   print $ runParser icecream f
+
 
