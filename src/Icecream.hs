4224c32650eaf1d89f0e15f9457ea3d613400584
Sandy Maguire <sandy@sandymaguire.me>
2017-07-09 16:07:57 -0600

    we did a good

--- src/Icecream.hs
+++ src/Icecream.hs
@@ -1,9 +1,29 @@
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE TemplateHaskell   #-}
 
 module Icecream where
 
 import Parser
 import BasePrelude hiding (try)
+import qualified Data.Map as M
+import Control.Lens
+
+
+data FuncDecl = FuncDecl
+  { fdId       :: String
+  , fdTypeDecl :: Maybe Type
+  , fdFuncs    :: [Func]
+  } deriving Show
+
+
+data TLD = TypeDeclTLD TypeDecl
+         | FuncTLD Func
+         deriving Show
+
+getRelevantName :: TLD -> String
+getRelevantName (TypeDeclTLD td) = tdId td
+getRelevantName (FuncTLD f)      = fId f
+
 
 
 data TypeDecl = TypeDecl { tdId :: String, tdType :: Type }
@@ -13,11 +33,13 @@ data Type = TypeCtr String
           | TypeArr Type Type
           deriving Show
 
-data Func = Func { fID :: String, fBinds :: [String], fBody :: Expr }
+data Func = Func { fId :: String, fBinds :: [String], fBody :: Expr }
           deriving Show
 
 type Expr = String
-           
+
+makePrisms ''TLD
+
 
 typeParser :: Parser Type
 typeParser = do
@@ -36,9 +58,19 @@ ident = trimming $ do
   c <- satisfy isAlpha
   cs <- many $ satisfy isAlpha
   return $ c : cs
-  
 
-icecream = many parseEverything
+
+icecream = makeFuncDecls <$> many parseTLD
+
+parseTLD :: Parser TLD
+parseTLD = do
+  trimmerDinger
+  r <- oneOf [ TypeDeclTLD <$> parseTypeD
+             , FuncTLD     <$> parseFunc
+             ]
+  trimmerDinger
+  return r
+
 
 parseTypeD :: Parser TypeDecl
 parseTypeD = do
@@ -48,24 +80,38 @@ parseTypeD = do
     return $ TypeDecl a b
 
 parseFunc :: Parser Func
-parseFunc = do 
+parseFunc = do
     a <- ident
     b <- many $ ident
     trimming $ char '='
     c <- ident
     return $ Func a b c
 
-parseEverything :: Parser (TypeDecl, Func)
-parseEverything = do
-    a <- parseTypeD
-    trimming $ char '\n'
-    b <- parseFunc
-    many $ char '\n'
-    return (a, b)
+trimmerDinger :: Parser ()
+trimmerDinger = void . many $ char '\n'
 
 main :: IO ()
 main = do
   f <- readFile "src/test.icecream"
   print $ runParser icecream f
+
+
+
+makeFuncDecls :: [TLD] -> [FuncDecl]
+makeFuncDecls = fmap (uncurry makeFuncDecls2)
+              . M.toList
+              . M.fromListWith (++)
+              . fmap (\tld -> (getRelevantName tld, [tld]))
+
+
+
+-- TODO(sandy): they are all the same relevant name
+makeFuncDecls2 :: String -> [TLD] -> FuncDecl
+makeFuncDecls2 name tlds =
+  let (yes, nos) = partition (has _TypeDeclTLD) tlds
+   in FuncDecl name
+               (fmap (\(TypeDeclTLD t) -> tdType t) $ listToMaybe yes)
+               (fmap (\(FuncTLD f) -> f) nos)
+
 
 
